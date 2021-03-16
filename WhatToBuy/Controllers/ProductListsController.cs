c083b3e596d1668199138e837c155d9cb080cf8f
Kirsart <47006507+Kirsart@users.noreply.github.com>
2021-03-16 23:29:08 +0400

    добавил контроллер листов продуктов

---
+++ WhatToBuy/Controllers/ProductListsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhatToBuy.Data;
+using WhatToBuy.Models;
+
+namespace WhatToBuy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductListsController : ControllerBase
+    {
+        private readonly WhatToBuyContext _context;
+
+        public ProductListsController(WhatToBuyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductLists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductList>>> GetProductList()
+        {
+            return await _context.ProductLists.ToListAsync();
+        }
+
+        // GET: api/ProductLists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductList>> GetProductList(Guid id)
+        {
+            var productList = await _context.ProductLists.FindAsync(id);
+
+            if (productList == null)
+            {
+                return NotFound();
+            }
+
+            return productList;
+        }
+
+        // PUT: api/ProductLists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductList(Guid id, ProductList productList)
+        {
+            if (id != productList.ProductListId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductLists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductList>> PostProductList(ProductList productList)
+        {
+            _context.ProductLists.Add(productList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductList", new { id = productList.ProductListId }, productList);
+        }
+
+        // DELETE: api/ProductLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductList(Guid id)
+        {
+            var productList = await _context.ProductLists.FindAsync(id);
+            if (productList == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductLists.Remove(productList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductListExists(Guid id)
+        {
+            return _context.ProductLists.Any(e => e.ProductListId == id);
+        }
+    }
+}
--- WhatToBuy/Startup.cs
+++ WhatToBuy/Startup.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WhatToBuy.Data;
+using WhatToBuy.Controllers;
 
 namespace WhatToBuy
 {
@@ -26,12 +22,14 @@ namespace WhatToBuy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WhatToBuy", Version = "v1" });
             });
+
+            services.AddDbContext<WhatToBuyContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WhatToBuyContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +42,6 @@ namespace WhatToBuy
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WhatToBuy v1"));
                 app.UseHttpsRedirection();
             }
-
 
             app.UseRouting();
 
